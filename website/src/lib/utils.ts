9d965c0a45da841365163d0e3fe47a9af02c14ae
Ersin KOÇ <ersinkoc@gmail.com>
2026-07-20 16:18:36 +0300

    release: bump version to 0.293.0

--- website/src/lib/utils.ts
+++ website/src/lib/utils.ts
@@ -12,7 +12,7 @@ export function cn(...inputs: ClassValue[]) {
    ========================================================================= */
 
 export const META = {
-  version: '0.292.1',
+  version: '0.293.0',
   repo: 'https://github.com/WrongStack/WrongStack',
   npm: 'wrongstack',
   node: '22',
